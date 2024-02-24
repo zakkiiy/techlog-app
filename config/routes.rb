39ae607cc54a843237b6ed600695ccd978f3fbe3
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2024-02-25 00:29:38 +0900

    Home#top のテストをリファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  root 'home#top'
+  # get 'home/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
